af70dbc896fcaff125f36af55213297aed0daaa1
Saeed Molaee <saeedmolaiy.dev@gmail.com>
2023-05-07 14:59:40 +0330

    #4 added config manager

---
+++ src/BetterRegex.Common/Configs/ConfigManager.cs
@@ -0,0 +1,36 @@
+﻿namespace BetterRegex.Common.Configs;
+
+public class ConfigManager
+{
+    public bool ShouldIncludeAsiaMobilePatterns { get; private set; }
+
+    public bool ShouldIncludeAmericaMobilePatterns { get; private set; }
+
+    public bool ShouldIncludeAfricaMobilePatterns { get; private set; }
+
+    public bool ShouldIncludeEuropeMobilePatterns { get; private set; }
+
+    public ConfigManager IncludeAsiaMobilePatterns()
+    {
+        ShouldIncludeAsiaMobilePatterns = true;
+        return this;
+    }
+
+    public ConfigManager IncludeAmericaMobilePatterns()
+    {
+        ShouldIncludeAmericaMobilePatterns = true;
+        return this;
+    }
+
+    public ConfigManager IncludeAfricaMobilePatterns()
+    {
+        ShouldIncludeAfricaMobilePatterns = true;
+        return this;
+    }
+
+    public ConfigManager IncludeEuropeMobilePatterns()
+    {
+        ShouldIncludeEuropeMobilePatterns = true;
+        return this;
+    }
+}
--- src/BetterRegex/Context/BetterRegex.cs
+++ src/BetterRegex/Context/BetterRegex.cs
@@ -1,21 +1,24 @@
 ﻿using BetterRegex.Factories;
 using BetterRegex.Infrastructure.Interfaces;
 using BetterRegex.Types;
+using BetterRegex.Common.Configs;
 
-namespace BetterRegex.Context
+namespace BetterRegex.Context;
+
+public static class BetterRegex
 {
-    public static class BetterRegex
+    public static readonly ConfigManager Configs;
+
+    private static readonly ValidatorFactory ValidatorFactory;
+
+    static BetterRegex()
     {
-        private static readonly ValidatorFactory ValidatorFactory;
+        Configs = new ConfigManager();
+        ValidatorFactory = new ValidatorFactory();
+    }
 
-        static BetterRegex()
-        {
-            ValidatorFactory = new ValidatorFactory();
-        }
-
-        public static IRegexValidator CreateRegexValidator(RegexValidatorTypes validatorType)
-        {
-            return ValidatorFactory.GetRegexValidator(validatorType);
-        }
+    public static IRegexValidator CreateRegexValidator(RegexValidatorTypes validatorType)
+    {
+        return ValidatorFactory.GetRegexValidator(validatorType);
     }
 }
