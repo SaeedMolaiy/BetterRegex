798b660a47dbe532084fc90a71311ec6838d1430
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-05-11 06:41:08 -0700

    #15 added html and sql regex patterns

--- src/BetterRegex.Definition/CultureManager.cs
+++ src/BetterRegex.Definition/CultureManager.cs
@@ -42,6 +42,17 @@ public class CultureManager
 
     public class SqlPatterns
     {
+        public const string SqlCommentRegex = "--.*$";
+        public const string SqlStringLiteralRegex = "'[^']*'";
+        public const string SqlIdentifierRegex = "\b[a-zA-Z_][a-zA-Z0-9_]*\b";
+        public const string SqlNumberRegex = "\\b[+-]?\\d+(\\.\\d+)?\\b";
+
+        public const string SqlKeywordRegex = "\b(SELECT|FROM|WHERE|JOIN|GROUP BY|ORDER BY|INSERT INTO|VALUES|UPDATE|SET|DELETE " +
+                                              "FROM|CREATE TABLE|ALTER TABLE|DROP TABLE|AND|OR|NOT|IN|LIKE)\b";
+
+        public const string SqlOperatorRegex = "[\\+\\-\\*/=<>]";
+        public const string SqlFunctionRegex = "\b(COUNT|SUM|AVG|MAX|MIN|DATEPART|DATEDIFF|GETDATE|NOW)\b";
+        public const string SqlStatementRegex = "\b(SELECT|INSERT INTO|UPDATE|DELETE FROM|CREATE TABLE|ALTER TABLE|DROP TABLE)\b";
     }
 
     public class EmailPatterns
@@ -58,6 +69,16 @@ public class CultureManager
 
     public class HtmlPatterns
     {
+        public const string HtmlTagRegex = "<([a-zA-Z][a-zA-Z0-9]*)\\b[^>]*>.*?</\\1>";
+        public const string HtmlAttributeRegex = "(?<=\\s|^)(\\w+)(?==['\"]?(([^'\"\\s]+)|(['\"])([^'\"]+)\\4))(?=\\s|$)";
+        public const string HtmlCommentRegex = "<!--(.*?)-->";
+        public const string HtmlEntityRegex = "&[a-zA-Z][a-zA-Z0-9]*;";
+        public const string HtmlLinkRegex = "<a\\b[^>]+href=['\"](.*?)['\"][^>]*>.*?</a>";
+        public const string HtmlImageRegex = "<img\\b[^>]+src=['\"](.*?)['\"][^>]*>";
+        public const string HtmlStyleRegex = "<style\b[^>]*>(.*?)</style>";
+        public const string HtmlHeadingRegex = "<h[1-6]\b[^>]*>.*?</h[1-6]>";
+        public const string HtmlScriptRegex = "<script\b[^>]*>(.*?)</script>";
+        public const string HtmlListRegex = "<(ul|ol)\\b[^>]*>.*?</\\1>";
     }
 
     private void InitializeMobileNumberDirectory()
