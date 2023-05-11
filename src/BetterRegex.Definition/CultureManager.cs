3d274344b2fe1a88200ee050e29940037670d7e8
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-05-11 02:54:43 -0700

    #14 added url and email patterns

--- src/BetterRegex.Definition/CultureManager.cs
+++ src/BetterRegex.Definition/CultureManager.cs
@@ -28,6 +28,16 @@ public class CultureManager
 
     public class UrlPatterns
     {
+        public const string HttpUrlRegex = "^(http|https)://[^\\s/$.?#].[^\\s]*$";
+        public const string FtpUrlRegex = "^(ftp)://[^\\s/$.?#].[^\\s]*$";
+        public const string FilePathUrlRegex = "^(file)://[^\\s/$.?#].[^\\s]*$";
+        public const string EmailUrlRegex = "^(mailto)://[^\\s/$.?#].[^\\s]*$";
+        public const string CustomProtocolUrlRegex = "^[a-zA-Z]+://[^\\s/$.?#].[^\\s]*$";
+        public const string DomainNameRegex = "^([a-zA-Z0-9]+(-[a-zA-Z0-9]+)*\\.)+[a-zA-Z]{2,}$";
+        public const string IpAddressIPv4 = "^\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b$";
+        public const string IpAddressIPv6 = "^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$";
+        public const string UrlWithQueryParamsRegex = "^(http|https)://[^\\s/$.?#].[^\\s]*\\?[^\\s]*$";
+        public const string UrlWithFragmentRegex = "^(http|https)://[^\\s/$.?#].[^\\s]*#[^\\s]*$";
     }
 
     public class SqlPatterns
@@ -36,6 +46,14 @@ public class CultureManager
 
     public class EmailPatterns
     {
+        public const string SimpleEmailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        public const string SubDomainEmailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}\\.[a-zA-Z]{2,}$";
+        public const string CaseInsensitiveEmailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        public const string EmailWithDisplayNameRegex = "^\"[\\w\\s]+\" <[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}>$";
+        public const string InternationalDomainEmailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(?:\\.[a-zA-Z]{2,})?$";
+        public const string CountryCodeTldEmailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2}$";
+        public const string ThreeLetterCountryCodeTldEmailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{3}$";
+        public const string IpAddressDomainEmailRegex = "^[a-zA-Z0-9._%+-]+@[0-9]+(\\.[0-9]+){3}$";
     }
 
     public class HtmlPatterns
